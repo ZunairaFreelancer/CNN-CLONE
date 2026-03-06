e4bb1d36ea970e9313e94e1ec47f1c499a630c2c
ZunairaFreelancer <dubaiislife.20@gmail.com>
2026-03-06 06:01:09 +0400

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,11 @@
+</div> <!-- End Container -->
+
+<footer>
+    <div class="container">
+        <p>&copy; <?php echo date("Y"); ?> CNN Clone. All rights reserved.</p>
+        <p>Designed for Excellence.</p>
+    </div>
+</footer>
+
+</body>
+</html>
